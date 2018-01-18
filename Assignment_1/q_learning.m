06b5384b527ddb3b0c1dc72c27b6b51f1792603d
siddsax <ss.siddharthasaxena@gmail.com>
2018-01-18 21:16:07 +0530

    error corrected on updating Q

--- Assignment_1/q_learning.m
+++ Assignment_1/q_learning.m
@@ -11,30 +11,36 @@ V = zeros(1,length(vec));
 Q = zeros(4,length(vec));
 
 % defining the inital position randomly
-curr = 24%ceil(rand*length(mat)^2);
+curr = ceil(rand*length(mat)^2);
 
 % list of legal actions at this position
 possible_actions = legal_actions(curr,mat);
 
 rew = 0;
-iters = 100;
+iters = 10000;
 for time = 1:iters
 % reward at present position
-    Q_val = -1000;
-    action_optimal = 0;
-    epsln = binornd(1,.5 + time/(2*iters));
-    if(epsln==1);
-        for indx = 1:length(possible_actions)
-            pos = update_position(curr,possible_actions(indx),mat);
-            
-            if(Q(possible_actions(indx),curr)>Q_val);
-                action_optimal = possible_actions(indx);
-            end;
+    Q_val = -inf;
+    action_optimal_1 = 0;
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Epsilon Greedy Policy %%%%%%%%%%%%%%%%%%%%%%%% 
+    epsln = binornd(1,.9);%8 + time/(5*iters));
+    for indx = 1:length(possible_actions)
+        pos = update_position(curr,possible_actions(indx),mat);
+        
+        if(Q(possible_actions(indx),curr)>Q_val);
+            action_optimal_1 = possible_actions(indx);
+            Q_val = Q(possible_actions(indx),curr);
         end;
+    end;
+    action_optimal_2 = possible_actions(ceil(rand*length(possible_actions)));
+    if(epsln==1);
+        action_optimal = action_optimal_1;
     else;
-        action_optimal = possible_actions(ceil(rand*length(possible_actions)));
+        action_optimal = action_optimal_2;
     end;    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
     R = vec(curr);
+    rew = rew + R;
     % choosing a random move
     new = update_position(curr,action_optimal,mat);
     % updating the expected future reward at present position
@@ -44,25 +50,34 @@ for time = 1:iters
         possible_actions = legal_actions(curr,mat);
 
     else;
-        % display(action_now)
-        % display(curr)
-        % display('----------')
-        % display(action_optimal);
-        % display(new)
-        Q(action_now,curr) = Q(action_now,curr) + alpha*(R + Q(action_optimal,new) - Q(action_now,curr));
-        V(curr) = sum(Q(:,curr))/length(possible_actions);
+
+        % best action at s'
+        t1 = legal_actions(new,mat);
+        t2 = -inf;
+        for indx = 1:length(t1)
+            pos = update_position(new,t1(indx),mat);
+            
+            if(Q(t1(indx),new)>t2);
+                t3 = t1(indx);
+                t2 = Q(t1(indx),new);
+            end;
+        end;
+        % Q(s,a) = (1-alpha)Q(s,a) + alpha*(R + gamma*(max over actions at s' Q(a',s'))) 
+        Q(action_now,curr) = (1-alpha)*Q(action_now,curr) + alpha*(R + gamma*Q(t3,new));
+        % V = \sum[probab_i*action_i]
+        V(curr) = epsln*Q(action_optimal_1,curr) + (1-epsln)*(sum(Q(:,curr))-Q(action_optimal_1,curr))/(length(possible_actions)-1);
         action_now = action_optimal;      
         curr = new;
         possible_actions = legal_actions(curr,mat);
         Vmat = reshape(V,length(mat), length(mat));
-        % TD Learning Error
+        % Learning Error
         error(time) = sqrt(sum((mat(mat~=0) - Vmat(mat~=0)).^2));    
         cumR(time) = R;
-        rew = rew + R;
     end;
 end;
-display(rew);
+disp(rew)
+display(Vmat);
  plot(error);
  plot(cumR);
-  % imagesc(Vmat);    
+  imagesc(Vmat);    
 % close(vidObj);
